7a9e9739430a3d40acad3e652ef78c25097a66e9
Ubuntu <rtelles64@gmail.com>
2020-10-09 21:36:55 +0000

    Add config file to project folder

---
+++ app/config.py
@@ -0,0 +1,10 @@
+# The config file for the flask app
+
+import os basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
